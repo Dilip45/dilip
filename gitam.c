f2420e298013548f68c740d1a6644223c8d9e874
Dilip45 <58928941+Dilip45@users.noreply.github.com>
2019-12-19 12:03:52 +0530

    Add files via upload

---
+++ gitam.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+
+printf("\n GITAM UNIVERSITY");
+goto a;
+printf("\n DYANAHITHA PRFESSIONAL OF STUDIES ");
+PRINTF("\n PROMIZEIT SERVICE PVT.LTD");
+a:
+	printf("\nn HYDERABAD");
+	return 0;
+}
