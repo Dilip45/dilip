17c7ab34571a6bf4756e20001c3a6174c80f76d0
Dilip45 <58928941+Dilip45@users.noreply.github.com>
2019-12-19 10:34:15 +0530

    Add files via upload

---
+++ 1 to n.c
@@ -0,0 +1,13 @@
+/*WAP to print 1 to n numbers*/
+
+#include<stdio.h>
+int main()
+{
+int n,i;
+scanf("%d",&n);
+for(i=1;i<=n;i++)
+{
+	printf("%d\t",i);
+}
+return 0;
+}
---
+++ comtrol statements.cpp
@@ -0,0 +1,13 @@
+while loop;
+while(condition)
+{
+	/*set of statements*/
+}
+
+do while loop;
+do
+{
+	/*set of statements*/
+}while(condition);
+
+
